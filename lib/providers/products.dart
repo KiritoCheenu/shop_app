6048c5a0e13d00e688169804625009141b4bd65b
KiritoCheenu <61424879+KiritoCheenu@users.noreply.github.com>
2021-02-12 13:30:50 +0530

    Update products.dart

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -97,8 +97,8 @@ class Products with ChangeNotifier {
   }
 
   Future<void> addProduct(Product product) async {
-    const url =
-        'https://shop-app-84240-default-rtdb.firebaseio.com/products.json';
+    final url =
+        'https://shop-app-84240-default-rtdb.firebaseio.com/products.json?auth=$authToken';
     try {
       final response = await http.post(
         url,
@@ -130,7 +130,7 @@ class Products with ChangeNotifier {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
       final url =
-          'https://shop-app-84240-default-rtdb.firebaseio.com/products/$id.json';
+          'https://shop-app-84240-default-rtdb.firebaseio.com/products/$id.json?auth=$authToken';
       await http.patch(url,
           body: json.encode({
             'title': newProduct.title,
@@ -147,7 +147,7 @@ class Products with ChangeNotifier {
 
   Future<void> deleteProduct(String id) async {
     final url =
-        'https://shop-app-84240-default-rtdb.firebaseio.com/products/$id.json';
+        'https://shop-app-84240-default-rtdb.firebaseio.com/products/$id.json?auth=$authToken';
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
     _items.removeAt(existingProductIndex);
